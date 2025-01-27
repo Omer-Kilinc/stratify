6292256cf125f8b810258c8b7e6d5ee2118fd1ff
Omer Kilinc <omerkilincwork@gmail.com>
2025-01-27 21:36:45 +0400

    updated app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import sys
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
 
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
 
 app.exec()
